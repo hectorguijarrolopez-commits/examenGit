8a4bef68a7fbcbd5fae2b7047b1b49cbecc6d814
hectorguijarrolopez-commits <hectorguijarrolopez@gmail.com>
2026-02-12 12:06:33 +0100

    v2

--- multiplos.java
+++ multiplos.java
@@ -1,8 +1,9 @@
 public static void multiplos (int numeroMax) {
     for (int i=0; i<=numeroMax;i++){
-        if(i%49==0){
-            System.out.println("Multiplo de 49:" +i)
-        }else if(i%100==0){
+        if(i%7==0){
+            System.out.println("Multiplo de 7:" +i)
+        }
+        else if(i%100==0){
              System.out.println("Multiplo de 100:" +i)
         }
     }
